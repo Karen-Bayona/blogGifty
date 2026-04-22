a06e6275d94934594866ffa91adf9104f910893f
Karen <karenmbayonar@gmail.com>
2026-04-21 22:26:16 -0500

    Configuración de JWT y conexión exitosa a Mongo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const app = express();
 // Para que el servidor entienda datos en formato JSON
 app.use(express.json());
 // Verificar que el servidor funciona
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Conexión exitosa a MongoDB Atlas'))
+    .catch((error) => console.error('Error al conectar a MongoDB:', error));
+
 app.get('/', (req, res) => {
     res.send('Bienvenida a la API de Gifty');
 });
