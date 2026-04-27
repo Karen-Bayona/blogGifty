c75f3592ad0681dc97f840eb908f1c2b447d6efe
Laufranco <laurafrsofia@gmail.com>
2026-04-26 19:40:05 -0500

    se corrigió token

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,12 +3,10 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
     // Leer el token del encabezado
     const token = req.header('x-auth-token');
-
     // Revisar si no hay token
     if (!token) {
         return res.status(401).json({ msg: 'No hay token, permiso no válido' });
     }
-
     // Validar el token
     try {
         const cifrado = jwt.verify(token, process.env.JWT_SECRET);
@@ -18,7 +16,5 @@ module.exports = function(req, res, next) {
         next();
     } catch (error) {
         res.status(401).json({ msg: 'Token no válido' });
-    }
-
-    
+    } 
 };
