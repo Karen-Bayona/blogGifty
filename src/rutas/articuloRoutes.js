e1e907f364245643ace2f80510406179c7f73b03
Laufranco <laurafrsofia@gmail.com>
2026-04-26 19:19:12 -0500

    Se agregó, POST, PUT DELETE, GET

--- src/rutas/articuloRoutes.js
+++ src/rutas/articuloRoutes.js
@@ -2,22 +2,18 @@ const express = require('express');
 const router = express.Router();
 const articuloController = require('../controllers/articuloController');
 
-// Importamos el middleware de seguridad que hizo Karen
-// Asegúrate de que la ruta al archivo sea la correcta
 const auth = require('../middleware/auth'); 
 
-// --- RUTAS DE NEGOCIO ---
-
-// 1. Ver todos los artículos (Público: no necesita token)
+// 1. Ver todos los artículos
 router.get('/', articuloController.obtenerArticulos);
 
-// 2. Crear un artículo (Protegido: Laura, aquí aplicas la seguridad de Karen)
+// 2. Crear un artículo 
 router.post('/', auth, articuloController.crearArticulo);
 
-// 3. Actualizar un artículo (Protegido)
+// 3. Actualizar un artículo 
 router.put('/:id', auth, articuloController.actualizarArticulo);
 
-// 4. Borrar un artículo (Protegido)
-router.delete('/:id', auth, articuloController.borrarArticulo);
+// 4. Eliminar un artículo 
+router.delete('/:id', auth, articuloController.eliminarArticulo);
 
 module.exports = router;
