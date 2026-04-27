bbca155906c63cad7023dc8d7fae9183ec1bff01
Laufranco <laurafrsofia@gmail.com>
2026-04-26 19:44:25 -0500

    Se corrigue y agrega errores para determinar su estado

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,11 +1,17 @@
 module.exports = (err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    
+    // 1. Determinar el código de estado 
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    
+    // Si el error trae su propio status 
+    if (err.statusCode) statusCode = err.statusCode;
+
     console.error(`[Error de Infraestructura]: ${err.message}`);
 
+    // 2. Enviar la respuesta al cliente
     res.status(statusCode).json({
         msg: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+        // Solo mostramos el stack trace si NO estamos en producción
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
     });
 };
-
-module.exports = errorMiddleware;
