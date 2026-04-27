d5e39022b6495a38ef23ca9a0df615166a647cdd
Laufranco <laurafrsofia@gmail.com>
2026-04-26 19:08:49 -0500

    Implementación de obtener, crear, actualizar,Eliminar

--- src/controladores/gadgetControlador.js
+++ src/controladores/gadgetControlador.js
@@ -1,5 +1,6 @@
-const Gadget = require('../modelos/gadget');
+const Gadget = require('../modelos/gadget'); 
 
+// 1. Obtener 
 exports.obtenerGadgets = async (req, res) => {
     try {
         const gadgets = await Gadget.find();
@@ -9,12 +10,34 @@ exports.obtenerGadgets = async (req, res) => {
     }
 };
 
+// 2. Crear 
 exports.crearGadget = async (req, res) => {
     try {
         const nuevoGadget = new Gadget(req.body);
         await nuevoGadget.save();
-        res.status(201).json(nuevoGadget);
+        res.status(201).json({ mensaje: "Gadget creado con éxito", nuevoGadget });
     } catch (error) {
-        res.status(400).json({ mensaje: "Error al crear gadget" });
+        res.status(400).json({ error: "Error al crear gadget" });
+    }
+};
+// 3. Actualizar
+exports.actualizarGadget = async (req, res) => {
+    try {
+        const gadgetActualizado = await Gadget.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!gadgetActualizado) return res.status(404).json({ mensaje: "No encontrado" });
+        res.json({ mensaje: "Actualizado correctamente", gadgetActualizado });
+    } catch (error) {
+        res.status(400).json({ error: "Error al actualizar" });
+    }
+};
+
+// 4. Eliminar
+exports.eliminarGadget = async (req, res) => {
+    try {
+        const gadgetEliminado = await Gadget.findByIdAndDelete(req.params.id);
+        if (!gadgetEliminado) return res.status(404).json({ mensaje: "No encontrado" });
+        res.json({ mensaje: "Eliminado correctamente" });
+    } catch (error) {
+        res.status(400).json({ error: "Error al eliminar" });
     }
 };
